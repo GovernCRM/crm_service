c1029679d64aa635604873b6e8af1db1c4867cb0
Peter Odeny <odenypeter@gmail.com>
2025-03-11 11:40:00 +0100

    fix error

--- crm_service/urls.py
+++ crm_service/urls.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.urls import path, include, re_path
+from django.shortcuts import redirect
 
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
-
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -17,13 +17,13 @@ schema_view = get_schema_view(
     permission_classes=(permissions.AllowAny,),
 )
 
-
 urlpatterns = [
     re_path(r'^docs/swagger(?P<format>\.json|\.yaml)$',
             schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('docs/', schema_view.with_ui('swagger', cache_timeout=0),
          name='schema-swagger-ui'),
     path('admin/', admin.site.urls),
+    path('', lambda request: redirect('/health_check/')),
     path('health_check/', include('health_check.urls')),
     path(r'', include('crm.urls')),
 ]
