1d85cfe01535fc9a3f06e749c75eed90ea674f60
Peter Odeny <odenypeter@gmail.com>
2025-04-10 18:41:14 +0200

    fix error

--- lists/admin.py
+++ lists/admin.py
@@ -6,6 +6,6 @@ from .models import List
 @admin.register(List)
 class ListAdmin(admin.ModelAdmin):
     list_display = ('name', 'description', 'parent', 'user_uuid', 'created_at', 'updated_at')
-    search_fields = ('name',)
+    search_fields = ('name', 'description')
     list_filter = ('created_at',)
     ordering = ('-created_at',)
